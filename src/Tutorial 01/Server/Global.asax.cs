bff16f33248a973dd46a7decf9211e131f61262b
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-03-15 22:58:32 +0100

    Removed WebAPI from Empty MVC solution...

--- src/Tutorial 01/Server/Global.asax.cs
+++ src/Tutorial 01/Server/Global.asax.cs
@@ -16,7 +16,6 @@ namespace Server
 		{
 			AreaRegistration.RegisterAllAreas();
 
-			WebApiConfig.Register(GlobalConfiguration.Configuration);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 		}
