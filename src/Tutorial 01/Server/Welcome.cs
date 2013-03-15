21088351cf0f2bd319f96314ad55b69ac2045b00
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-03-16 00:51:05 +0100

    Added Welcome and WelcomeResponse

---
+++ src/Tutorial 01/Server/Welcome.cs
@@ -0,0 +1,7 @@
+﻿namespace cyberzed.TechPro.ServiceStack_01.Server
+{
+	public class Welcome
+	{
+		public string Name { get; set; }
+	}
+}
---
+++ src/Tutorial 01/Server/WelcomeResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace cyberzed.TechPro.ServiceStack_01.Server
+{
+	public class WelcomeResponse
+	{
+		public string Message { get; set; }
+	}
+}
