a1ad20352df188e496d81f69d57e84ff2ef192f8
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-03-16 00:06:21 +0100

    Added AppHost

---
+++ src/Tutorial 01/Server/AppHost.cs
@@ -0,0 +1,20 @@
+﻿using Funq;
+using ServiceStack.WebHost.Endpoints;
+
+namespace cyberzed.TechPro.ServiceStack_01.Server
+{
+	public class AppHost : AppHostBase
+	{
+		public AppHost() : base("TechPro ServiceStack Tutorial 01", typeof (AppHost).Assembly)
+		{
+		}
+
+		public override void Configure(Container container)
+		{
+			SetConfig(new EndpointHostConfig
+				{
+					ServiceStackHandlerFactoryPath = "api"
+				});
+		}
+	}
+}
