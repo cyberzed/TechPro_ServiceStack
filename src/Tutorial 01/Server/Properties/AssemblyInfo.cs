b6e83cda97bddbd6b2937d35327e32115572d228
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-03-15 23:03:48 +0100

    Complete Server project cleanup

--- src/Tutorial 01/Server/Properties/AssemblyInfo.cs
+++ src/Tutorial 01/Server/Properties/AssemblyInfo.cs
@@ -1,25 +1,27 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Server")]
+
+[assembly: AssemblyTitle("cyberzed TechPro ServiceStack Tutorial 01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Atea A/S")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Server")]
-[assembly: AssemblyCopyright("Copyright © Atea A/S 2013")]
+[assembly: AssemblyCopyright("Copyright © Stefan Daugaard Poulsen 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("91d94ed6-2adb-4a22-8c51-4a9adf39eacb")]
 
 // Version information for an assembly consists of the following four values:
@@ -31,5 +33,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
