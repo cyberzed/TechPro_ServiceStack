cfd460d33116fdbb85ba4c423b9958e0a45bb046
Stefan D. Poulsen <cyberzed@sleddog.dk>
2013-03-16 01:15:55 +0100

    Added DTO routing

--- src/Tutorial 01/Server/Welcome.cs
+++ src/Tutorial 01/Server/Welcome.cs
@@ -1,5 +1,9 @@
-﻿namespace cyberzed.TechPro.ServiceStack_01.Server
+﻿using ServiceStack.ServiceHost;
+
+namespace cyberzed.TechPro.ServiceStack_01.Server
 {
+	[Route("welcome")]
+	[Route("welcome/{Name*}")]
 	public class Welcome
 	{
 		public string Name { get; set; }
